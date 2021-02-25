939bf853b835206aa002690dda410df7856b68c4
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-02-25 17:11:41 +0200

    init repo

---
+++ main.py
@@ -0,0 +1,8 @@
+from matplotlib import pyplot as plt
+years = [1999,2000,2001,2002,2003]
+price = [100,230,424,542,345]
+with plt.style.context('dark_background'):
+    plt.plot(years,price,color="blue",marker='o')
+plt.title("Price evolution")
+plt.ylabel("$")
+plt.show()
